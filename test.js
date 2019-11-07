cdd67bcde67d1c3c5da871769c1a853183bbe2df
Matt <mattbond694@gmail.com>
2019-11-07 21:33:35 +0200

    Fixes test that was testing the wrong page

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test('responds to requests', (t) => {
   t.plan(3);
   // Wait for the server to shout that it started
   child.stdout.on('data', () => {
-    request(`http://127.0.0.1:${port}`, { timeout: 1500 }, (error, response, body) => {
+    request(`http://127.0.0.1:${port}/install`, { timeout: 1500 }, (error, response, body) => {
       t.false(error);
       t.equal(response.statusCode, 200);
       t.notEqual(body.indexOf('Application is installed and running. (go to /index.html to get started)'), -1);
